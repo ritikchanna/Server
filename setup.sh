9831e32639eb4cae2abc26ff8a2f598510acf1ee
Ritik Channa <chnritik@gmail.com>
2020-10-10 12:10:27 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt update
+sudo apt install apache2
+# Get HTTPS cert
+sudo snap install core; sudo snap refresh core
+sudo apt-get remove certbot -y
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo certbot --apache --domains ritik.gq
